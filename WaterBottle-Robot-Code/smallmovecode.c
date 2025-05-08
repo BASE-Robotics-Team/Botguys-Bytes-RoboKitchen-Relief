5f25d17240c2cd161b2448f1776af398f7166550
vatsalatea <96271833+vatsalatea@users.noreply.github.com>
2025-05-07 20:09:27 -0400

    added foward, backwards and motorclear functions

--- WaterBottle-Robot-Code/smallmovecode.c
+++ WaterBottle-Robot-Code/smallmovecode.c
@@ -1,6 +1,12 @@
 #include <kipr/wombat.h>
 #include <stdio.h>
+#include <stdlib.h>
 
+int motorfr = 2;
+int motorfl = 3;
+int motorbr = 1;
+int motorbl = 0;
+int speed = 100;
 int arm = 0;
 int second = 1000;
 int arm_current= 0;
@@ -14,13 +20,34 @@ void slow(port,end,current){
         set_servo_position(port,(sevcurrent += 5));
     }
 }
+void forward(){
+	motor(motorbr,speed);
+	motor(motorbl,speed);
+    motor(motorfl,speed);
+    motor(motorfr,speed);
+}
+void backward(){
+	motor(motorbr,-speed);
+	motor(motorbl,-speed);
+    motor(motorfl,-speed);
+    motor(motorfr,-speed);
+}
+void motorclear(motorfr,motorfl,motorbr,motorbl){
+    cmpc(motorfr)
+    cmpc(motorfl)
+    cmpc(motorbr)
+    cmpc(motorbl)
+    if((gmpc(motorfr))||(gmpc(motorfl))||(motorbr)||(motorbl)){exit(0);}
 
-
+}
 
 int main()
 {
+    motorclear(motorfr,motorfl,motorbr,motorbl);
     enable_servos();
     set_servo_position(arm,sevhm);
-    slow(arm,1700,arm_current)
+    slow(arm,1700,arm_current);
+    msleep(second*10);
+    ao();
     return 0;
 }
