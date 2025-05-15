38d33b595444ce337ba60e0bb138f26af9ef277a
vatsalatea <96271833+vatsalatea@users.noreply.github.com>
2025-05-15 10:07:23 -0400

    Update Threadingfunc.c
    added code

--- WaterBottle-Robot/Threadingfunc.c
+++ WaterBottle-Robot/Threadingfunc.c
@@ -1,1 +1,37 @@
+#include <kipr/wombat.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+// Struct to hold parameters for our motor threads
+typedef struct {
+    int port;
+    int speed;
+} motor_args;
+
+// Wrapper function for the first motor (motorbr)
+void *run_motorbr(void *args) {
+    motor_args *params = (motor_args *)args;
+    motor(params->port, params->speed);
+    return NULL;
+}
+
+// Wrapper function for the second motor (motorbl)
+void *run_motorbl(void *args) {
+    motor_args *params = (motor_args *)args;
+    motor(params->port, -params->speed); // Note the -speed
+    return NULL;
+}
+
+// Wrapper function for the third motor (motorfl)
+void *run_motorfl(void *args) {
+    motor_args *params = (motor_args *)args;
+    motor(params->port, params->speed);
+    return NULL;
+}
+
+// Wrapper function for the fourth motor (motorfr)
+void *run_motorfr(void *args) {
+    motor_args *params = (motor_args *)args;
+    motor(params->port, -params->speed); // Note the -speed
+    return NULL;
+}
