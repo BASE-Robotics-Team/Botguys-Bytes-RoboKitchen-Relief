820fbfb603ba0f58e970f38fef54040d0a9cd942
vatsalatea <96271833+vatsalatea@users.noreply.github.com>
2025-05-16 14:28:11 -0400

    Update main_code_current.c
    updated with new code

--- WaterBottle-Robot/main_code_current.c
+++ WaterBottle-Robot/main_code_current.c
@@ -16,26 +16,75 @@ int sevcurrent = 0;
 int sevcurrentv = 2047;
 int sevcurrent2 = 1600;
 int x = 4;
-// servo home postion change when new postion is set 
+// servo home postion change when new postion is set
 int sevhm = 0;
 
 
-int main() 
+int main()
 {
-    motorclear(motorbr,motorbl,motorfl,motorfr);
+    ao();
+    printf("START \n");
+    //wait_for_light(0);
+    shut_down_in(119);
+    enable_servo(3);
+    enable_servo(2);
     enable_servo(0);
+    chkserv(3,2,0,1);
+    ssp(0,0);
+    msleep(50);
+    ssp(2,1350);
+    ssp(3,320);
     msleep(500);
-    //chkserv(0,1,2,3);
-    //backward(motorbr,motorbl,motorfl,motorfr,100);
-    //forward(motorbr,motorbl,motorfl,motorfr,100);
-    //msleep(7000);
-    //stop(0);
-    //stop(1);
-    //stop(2);
-    //stop(3);
-    //msleep(1000);
-    crabr(motorbr,motorbl,motorfl,motorfr,10);
-    msleep(10000);
+    disable_servos();
+    msleep(50);
+    motorclear(motorbr,motorbl,motorfl,motorfr);
+    msleep(50);
+backward(motorbr,motorbl,motorfl,motorfr,speed);
+    msleep(600);
+    ao();
+    move_in_place_ccw(motorbr,motorbl,motorfl,motorfr,speed);
+    msleep(1500);
+    ao();
+    forward(motorbr,motorbl,motorfl,motorfr,speed);
+    msleep(3950);
+    ao();
+    enable_servo(3);
+    enable_servo(2);
+    chkserv(3,2,0,1);
+    msleep(25);
+    ssp(2,0);
+    ssp(3,1600);
+    msleep(1200);
+    move_in_place_ccw(motorbr,motorbl,motorfl,motorfr,speed);
+    msleep(1350);
+    ao();
+    printf("analog placeing start \n");
+    while( digital(9) != 1){
+        forward(motorbr,motorbl,motorfl,motorfr,10);
+    }
+        printf("analog placeing end \n");
+    msleep(290);
+    ao();
+    slow(portarm,portarm2,sevcurrent,sevcurrent2,1200);
+    msleep(2000);
+    backward(motorbr,motorbl,motorfl,motorfr,15);
+    msleep(400);
+    forward(motorbr,motorbl,motorfl,motorfr,15);
+    msleep(400);
+    ao();
+    printf("Phase 1 pass \n");
+    enable_servo(0);
+    chkserv(3,2,0,1);
+    msleep(25);
+    ssp(0,2047);
+    msleep(1500);
+    slowdwn(portarm,portarm2,700);
+    msleep(2500);
+    backward(motorbr,motorbl,motorfl,motorfr,35);
+    msleep(750);
+    ao();
+    move_in_place_ccw(motorbr,motorbl,motorfl,motorfr,45);
+    msleep(8000);
     ao();
     return 0;
 }
